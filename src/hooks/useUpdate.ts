0338fc9355b0ee9bea7f597cadb9d40e52a35b31
Aria Edmonds <aria@ar1as.space>
2021-06-08 20:44:07 +0800

    fix up sloppy useUpdate paste

--- src/hooks/useUpdate.ts
+++ src/hooks/useUpdate.ts
@@ -1,28 +1,31 @@
 import { useSupabase } from './useSupabase';
 import * as RD from '@devexperts/remote-data-ts';
 import * as TE from 'fp-ts/TaskEither';
-import { useState } from 'react';
 import { pipe } from 'fp-ts/lib/function';
 import { Filter } from '../types';
+import { useStable } from 'fp-ts-react-stable-hooks';
+import * as S from 'fp-ts/string';
+import * as E from 'fp-ts/Eq';
 
 export const useUpdate = <T = any>(
-  tableName: string
+  tableName: string,
+  eq: E.Eq<T[]> = E.eqStrict
 ): [
-  RD.RemoteData<string, T | T[]>,
+  RD.RemoteData<string, T[]>,
   (values: Partial<T>, filter: Filter<T>) => Promise<void>
 ] => {
   const supabase = useSupabase();
 
-  // TODO: Figure out a way to do useStable/Eq for T | T[]
-  const [result, setResult] = useState<RD.RemoteData<string, T | T[]>>(
-    RD.initial
+  const [result, setResult] = useStable<RD.RemoteData<string, T[]>>(
+    RD.initial,
+    RD.getEq(S.Eq, eq)
   );
 
   const execute = async (values: Partial<T>, filter: Filter<T>) => {
     setResult(RD.pending);
     pipe(
       supabase,
-      TE.fromOption(() => 'You must use useUpsert from inside a Provider!'),
+      TE.fromOption(() => 'You must use useUpdate from inside a Provider!'),
       TE.chainTaskK(supabase => async () =>
         await filter(supabase.from<T>(tableName).update(values))
       ),
